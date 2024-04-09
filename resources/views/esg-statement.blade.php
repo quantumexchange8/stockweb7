0cd1045d3fb397bc1507564feb364e966536793f
tonglim <tonglim88@gmail.com>
2024-04-09 14:39:51 +0800

    update

--- resources/views/esg-statement.blade.php
+++ resources/views/esg-statement.blade.php
@@ -21,36 +21,66 @@
     </div>
 </div>
 
+<!--TITLE AND INTRO-->
 <div class="page-section">
-    <div class="container bs-docs-container p-140-cont">
+    <div class="container">
         <div class="row">
-            <div class="col-md-11 col-md-offset-0">
-                <div id="tabs" class="mb-45 bs-docs-section text-center">
-                    <div class="heading-underline h3-line">
-                        <div class="row">
-                            <div class="col-md-11 col-md-offset-1">
-                                <ul id="myTab" class="nav nav-tabs bootstrap-tabs">
-                                    <li  class="active"><a href="#overview" class="a-inv" data-toggle="tab">Overview</a></li>
-                                    <li><a href="#environment" class="a-inv" data-toggle="tab">Environment</a></li>
-                                    <li><a href="#social" class="a-inv" data-toggle="tab">Social</a></li>
-                                    <li><a href="#governance" class="a-inv" data-toggle="tab">Governance</a></li>
-                                </ul>
-                            </div>
+            <div class="col-md-12 pt-100 wow fadeInLeft">
+                
+                    <div class="title-fs-45 uppercase">
+                      <span class="bold">ESG STATEMENT</span> 
+                    </div>
+                    <div class="line-3-100"></div>
+                    <p >
+                        Corporations are placing more significance on ESG obligations, which encompass environmental, social, and governance aspects. These agreements demonstrate a company's unwavering commitment to integrating sustainability, social responsibility, and ethical governance standards into its investing plans.
+                      </p>
+                      <p >Investment businesses that prioritise ESG commitments seek to invest in companies that exhibit robust environmental practices, including carbon emissions reduction, resource conservation, and promotion of renewable energy. In addition, they take into account social aspects such as labour practices, diversity and inclusion, and community engagement. Additionally, the assessment considers governance principles such as clear and open reporting, a diverse board, and ethical leadership.
+                      </p>
+                      <p >By incorporating environmental, social, and governance (ESG) factors into their investment choices, companies can ensure that their portfolios are in line with sustainable and ethical principles. This technique not only helps to reduce risks related to environmental and social issues but also contributes to the production of long-term value. Investors are progressively acknowledging the significance of ESG commitments and are actively searching for investment firms that prioritise these elements.</p>
+                      <p >ESG commitments for investment businesses encompass the integration of environmental, social, and governance factors into their investing strategies, with the aim of fostering sustainability, social responsibility, and ethical conduct. </p>           
+                </div>
+        </div>
+    </div>
+</div>
+
+<div class="page-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 left-50">
+                    <div class="fes2-main-text-cont">
+                        <div class="title-fs-45">
+                            Environment</span> 
                         </div>
+                        <div class="line-3-100"></div>
+                        <p>Taking on the role of stewards for future generations, {{ config('app.full_company_name') }} will:</p>
+                        <p>
+                          <ul class="icon-list">
+                              <li><i class="fa fa-angle-double-right"></i>Reduce the impact of our operations in order to combat climate change, pollution, and other negative environmental impacts.
+                              </li>
+                              <li><i class="fa fa-angle-double-right"></i>Include ecologically responsible and sustainable techniques in each and every one of the services that we offer to our customers.</li>
+                          </ul>
+                        </p>
+                    </div>
+            </div>
+                <div class="col-md-6 right-50 wow fadeInLeft">
+                    <div class="ads-img-cont" >
+                        <img src="{{asset('assets/images/environment.jpg')}}" alt="img">
                     </div>
                 </div>
-                <div id="myTabContent" class="tab-content col-md-offset-1">
-                    <div class="tab-pane fade text-color-darkgrey" id="environment">
-                      <p>Taking on the role of stewards for future generations, {{ config('app.full_company_name') }} will:</p>
-                      <p>
-                        <ul class="icon-list">
-                            <li><i class="fa fa-angle-double-right"></i>Reduce the impact of our operations in order to combat climate change, pollution, and other negative environmental impacts.
-                            </li>
-                            <li><i class="fa fa-angle-double-right"></i>Include ecologically responsible and sustainable techniques in each and every one of the services that we offer to our customers.</li>
-                        </ul>
-                      </p>
-                    </div>
-                    <div class="tab-pane fade text-color-darkgrey" id="social">
+        </div>
+    </div>
+</div> 
+<hr class="mt-0 mb-0">	
+
+<div class="page-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6  ">
+                    <div class="fes2-main-text-cont">
+                        <div class="title-fs-45">
+                            Social
+                        </div>
+                        <div class="line-3-100"></div>
                         <ul class="icon-list">
                             <li><i class="fa fa-angle-double-right"></i>Maintain a culture of integrity that holds {{ config('app.full_company_name') }} and its employees accountable to the highest ethical standards, and continue to bring this culture into existence.
                             </li>
@@ -64,7 +94,25 @@
                         </ul>
                         <p>By participating in philanthropic events and volunteering, we may show our support for our communities.</p>
                     </div>
-                    <div class="tab-pane fade text-color-darkgrey" id="governance">
+            </div>
+            <div class="col-md-6 wow fadeInRight">
+                    <div class="ads-img-cont" >
+                        <img src="{{asset('assets/images/social.jpg')}}" alt="img">
+                    </div>
+            </div>
+        </div>
+    </div>
+</div>        
+<hr class="mt-0 mb-0">	
+<div class="page-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 left-50">
+                    <div class="fes2-main-text-cont">
+                        <div class="title-fs-45">
+                            Governance
+                        </div>
+                        <div class="line-3-100"></div>
                         <ul class="icon-list">
                             <li><i class="fa fa-angle-double-right"></i>Establish a committee that is accountable to the Board in order to establish and keep {{ config('app.full_company_name') }} accountable for its lofty environmental, social, and governance obligations
                             </li>
@@ -76,18 +124,14 @@
                             </li>
                         </ul>
                     </div>
-                    <div class="tab-pane fade in active text-color-darkgrey" id="overview">
-                        <p >
-                            Corporations are placing more significance on ESG obligations, which encompass environmental, social, and governance aspects. These agreements demonstrate a company's unwavering commitment to integrating sustainability, social responsibility, and ethical governance standards into its investing plans.
-                          </p>
-                          <p >Investment businesses that prioritise ESG commitments seek to invest in companies that exhibit robust environmental practices, including carbon emissions reduction, resource conservation, and promotion of renewable energy. In addition, they take into account social aspects such as labour practices, diversity and inclusion, and community engagement. Additionally, the assessment considers governance principles such as clear and open reporting, a diverse board, and ethical leadership.
-                          </p>
-                          <p >By incorporating environmental, social, and governance (ESG) factors into their investment choices, companies can ensure that their portfolios are in line with sustainable and ethical principles. This technique not only helps to reduce risks related to environmental and social issues but also contributes to the production of long-term value. Investors are progressively acknowledging the significance of ESG commitments and are actively searching for investment firms that prioritise these elements.</p>
-                          <p >ESG commitments for investment businesses encompass the integration of environmental, social, and governance factors into their investing strategies, with the aim of fostering sustainability, social responsibility, and ethical conduct. </p>           
-                    </div>
-                  </div>
             </div>
+                <div class="col-md-6 right-50 wow fadeInLeft">
+                    <div class="ads-img-cont" >
+                        <img src="{{asset('assets/images/governance.jpg')}}" alt="img">
+                    </div>
+                </div>
         </div>
     </div>
-</div>
+</div> 
+
 @endsection
